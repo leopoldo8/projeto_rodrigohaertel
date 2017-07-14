b0bd4fce969b4b490c02a4bed6010a2d3963b194
Rodrigo <rodrigo.haertel@yahoo.com.br>
2017-07-14 17:13:23 -0300

    JS 80% feito

--- Realiza Network/Escopo/structure/js/home.js
+++ Realiza Network/Escopo/structure/js/home.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var userData = localStorage.getItem('userData');
 userData = JSON.parse(userData);
 
@@ -77,52 +76,4 @@ function init() {
 	loadFeeds();
 }
 
-=======
-<<<<<<< HEAD
-var userData = localStorage.getItem('userData');
-userData = JSON.parse(userData);
-
-function loadUserData() {
-	$(".nameperfil").text(userData.first_name+" "+userData.last_name);
-	$(".iniciaisperfil").text(userData.first_name.charAt(0)+userData.last_name.charAt(0));
-	$(".emailperfil").text(userData.email);
-	$(".idadeperfil").text(userData.age);
-};
-
-$(".exit img").on("click", function sair() {
-	localStorage.removeItem('userData');
-	window.location = "login.html";	
-});
-
-$("#up").on("click", function() {$("html, body").animate({ scrollTop: 0 }, "slow"); return false;});
-
-$("#postar").on("submit", function postar() {
-	var userId = userData.id;
-	var texto = $("textearea").val();
-	$.ajax({
-
-	});
-});
-
-function init() {
-	loadUserData();
-}
-
-=======
-var userData = localStorage.getItem('userData');
-userData = JSON.parse(userData);
-
-function loadUserData() {
-	$(".nameperfil").text(userData.first_name+" "+userData.last_name);
-	$(".iniciaisperfil").text(userData.first_name.charAt(0)+userData.last_name.charAt(0));
-	$(".emailperfil").text(userData.email);
-	$(".idadeperfil").text(userData.age);
-};
-
-function init() {
-	loadUserData();
-}
-
->>>>>>> 958873c0b1e0c0e9a54124ff781b991ecf76f009
->>>>>>> ec533984b352c25f488e2b203252830774bfdde4
 init();
